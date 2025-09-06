b9845f96b707e70c8723b2c5bd96480969a064dc
Subrata Pramanik <154335629+SUBRATAlsdm@users.noreply.github.com>
2025-09-07 03:05:48 +0530

    addin uvicorn command

--- main.py
+++ main.py
@@ -42,6 +42,13 @@ app.include_router(admin_link.router)
 def root():
     return {"message": "Welcome to Portfolio FastAPI"}
 
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000))
+    )
+
 
 # from fastapi import FastAPI
 
